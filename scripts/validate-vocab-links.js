ba249f8c4ced8ed095c8243d9bb83e93f0f4745c
Tobias Winkler <7114819+Tschuck@users.noreply.github.com>
2024-10-22 10:41:00 +0200

    fix: vocab links

--- scripts/validate-vocab-links.js
+++ scripts/validate-vocab-links.js
@@ -1,30 +1,29 @@
 const fs = require('fs');
-const { vocabPaths, jsonLdSchemaPaths } = require('./files');
+const { jsonLdSchemaPaths } = require('./files');
 
 function checkSchemaLinks(jsonLdSchemaPath) {
   const jsonLdTextContent = fs.readFileSync(jsonLdSchemaPath, 'utf8');
-  for (let vocabPath of vocabPaths) {
-    const v1SchemaContent = fs.readFileSync(vocabPath, 'utf8');
 
-    const jsonLdV1Links = (jsonLdTextContent.match(/https\:\/\/oid.spherity.com.*#.*(?=")/g) || [])
+  const vocabPath = jsonLdSchemaPath.replace(`v1.jsonld`, 'vocabulary.md');
+  const v1SchemaContent = fs.readFileSync(vocabPath, 'utf8');
+  const jsonLdV1Links = (jsonLdTextContent.match(/https\:\/\/oid.spherity.com.*#.*(?=")/g) || [])
 
-    const missingLinks = jsonLdV1Links.filter((link) => {
-      const slug = link.split('#')[1];
-      const isFound = v1SchemaContent.includes(`{#${slug}}`);
+  const missingLinks = jsonLdV1Links.filter((link) => {
+    const slug = link.split('#')[1];
+    const isFound = v1SchemaContent.includes(`{#${slug}}`);
 
-      if (!isFound) {
-        console.log(`   ❌ ${slug}`);
-        return slug;
-      }
-
-      console.log(`   ✅ ${slug}`);
-    });
-
-    if (missingLinks.length) {
-      const missingAsText = missingLinks.map((link) => `\n- ${link}`).join('');
-
-      throw new Error(`Links present in v1/schema.jsonld but not in vocabularies/v1: \n${missingAsText}`);
+    if (!isFound) {
+      console.log(`   ❌ ${slug}`);
+      return slug;
     }
+
+    console.log(`   ✅ ${slug}`);
+  });
+
+  if (missingLinks.length) {
+    const missingAsText = missingLinks.map((link) => `\n- ${link}`).join('');
+
+    throw new Error(`Links present in v1/schema.jsonld but not in vocabularies/v1: \n${missingAsText}`);
   }
 
   console.log(`\n   ✅ All links are present in vocabularies/v1.md`);
